f09468a8a4a449697c8b2131e02681459bd1faae
IzzArk <faiz.arkan1061@gmail.com>
2023-03-02 21:24:51 +0700

    Menyelsaikan This

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
 
+
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    </BrowserRouter>
-   
-  </>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   )
 }
 
