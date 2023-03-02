f3485d2b393605956b1576e9c4c7450871781c81
m1kael1 <mikaelagungdjawa@gmail.com>
2023-03-02 23:39:19 +0700

    membuat komponen brands pada home

--- src/components/Home/Brand.jsx
+++ src/components/Home/Brand.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import cisco from "../../assets/Brands/cisco.svg";
+import hp from "../../assets/Brands/hp.svg";
+import huawei from "../../assets/Brands/huawei.svg";
+import juniper from "../../assets/Brands/juniper.svg";
 
 const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+	return (
+		<div className="container-brands w-full min-w-300 h-[315px] flex items-center justify-center">
+			<div className="flex justify-center gap-[56px] min-w-300 w-[910px] h-[100px] items-center">
+				<div>
+					<img src={cisco} alt="" />
+				</div>
+				<div>
+					<img src={huawei} alt="" />
+				</div>
+				<div>
+					<img src={hp} alt="" />
+				</div>
+				<div>
+					<img src={juniper} alt="" />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Brand
+export default Brand;
