8442197593db9e7d5f2ac152d5137ccbe0503067
m1kael1 <mikaelagungdjawa@gmail.com>
2023-03-03 13:32:47 +0700

    membuat brands

--- src/components/Home/Brand.jsx
+++ src/components/Home/Brand.jsx
@@ -3,22 +3,31 @@ import cisco from "../../assets/Brands/cisco.svg";
 import hp from "../../assets/Brands/hp.svg";
 import huawei from "../../assets/Brands/huawei.svg";
 import juniper from "../../assets/Brands/juniper.svg";
+import "../../styles/Brand.css";
 
 const Brand = () => {
 	return (
 		<div className="container-brands w-full min-w-300 h-[315px] flex items-center justify-center">
 			<div className="flex justify-center gap-[56px] min-w-300 w-[910px] h-[100px] items-center">
-				<div>
-					<img src={cisco} alt="" />
+				<div className="cisco w-[184px] h-[100px]">
+					<a href="https://www.cisco.com/">
+						<img src={cisco} alt="cisco" />
+					</a>
 				</div>
-				<div>
-					<img src={huawei} alt="" />
+				<div className="huawei w-[100px] h-[100px]">
+					<a href="https://www.huawei.com/">
+						<img src={huawei} alt="huawei" />
+					</a>
 				</div>
-				<div>
-					<img src={hp} alt="" />
+				<div className="hp w-[100px] h-[100px]">
+					<a href="https://www.hp.com/">
+						<img src={hp} alt="hp" />
+					</a>
 				</div>
-				<div>
-					<img src={juniper} alt="" />
+				<div className="juniper w-[369px] h-[100px]">
+					<a href="https://www.juniper.net/">
+						<img src={juniper} alt="juniper" />
+					</a>
 				</div>
 			</div>
 		</div>
--- src/components/Home/This.jsx
+++ src/components/Home/This.jsx
@@ -5,23 +5,25 @@ import "../../styles/This.css";
 const This = () => {
 	return (
 		<div className="this">
-			<div className="this-pict">
-				<img src={Pict} />
-			</div>
-			<div className="this-text">
-				<h1>KENAPA HARUS KITA</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor
-					bibendum diam id volutpat. Sed laoreet lacus quis sagittis malesuada.
-					Morbi sollicitudin lectus a eros fermentum eleifend. Phasellus dictum
-					quam a tortor tempus fermentum. Cras aliquam justo dui, quis aliquam
-					purus iaculis ac. Etiam vel diam efficitur, rutrum tortor in, tempor
-					odio. Curabitur non erat vitae libero varius vestibulum. Integer vel
-					neque a neque pulvinar dignissim in ac magna. Fusce ac scelerisque
-					turpis. Mauris lacinia euismod tellus vitae tincidunt. Donec vel
-					volutpat est. Pellentesque ut libero molestie, iaculis massa quis,
-					luctus elit.
-				</p>
+			<div className="this-container">
+				<div className="this-pict">
+					<img src={Pict} />
+				</div>
+				<div className="this-text">
+					<h1>KENAPA HARUS KITA</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+						auctor bibendum diam id volutpat. Sed laoreet lacus quis sagittis
+						malesuada. Morbi sollicitudin lectus a eros fermentum eleifend.
+						Phasellus dictum quam a tortor tempus fermentum. Cras aliquam justo
+						dui, quis aliquam purus iaculis ac. Etiam vel diam efficitur, rutrum
+						tortor in, tempor odio. Curabitur non erat vitae libero varius
+						vestibulum. Integer vel neque a neque pulvinar dignissim in ac
+						magna. Fusce ac scelerisque turpis. Mauris lacinia euismod tellus
+						vitae tincidunt. Donec vel volutpat est. Pellentesque ut libero
+						molestie, iaculis massa quis, luctus elit.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
