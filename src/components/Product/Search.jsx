593af7003f7d001eb56a35a8fb16f1d7e5c1b1fa
IzzArk <faiz.arkan1061@gmail.com>
2023-03-15 22:41:40 +0700

    selesai search

--- src/components/Product/Search.jsx
+++ src/components/Product/Search.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../../styles/Search.css'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+      <div className='main-search'>
+        <div className='search-box'>
+          <input
+            type="text"
+            placeholder="Mencari Barang..." />
+          <div className='green-search'>
+            <span class="material-symbols-outlined">
+              search
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
