404b55a6b168021b7abed0a8f28f1457668506f0
Mikael  Agung Djawa <94010783+m1kael1@users.noreply.github.com>
2023-04-01 00:08:38 +0700

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Search from "../components/Product/Search";
 import ProductSection from "../components/Product/ProductSection";
-import FooterProduct from "../components/Product/FooterProduct";
+import Footer from "../components/Footer";
 
 const Product = () => {
 	return (
 		<div>
 			<Search></Search>
 			<ProductSection></ProductSection>
-			<FooterProduct></FooterProduct>
+			<Footer></Footer>
 		</div>
 	);
 };
