fc2babc6785e12f1e5bb7757b7a7ffcc3f3cb4f8
m1kael1 <mikaelagungdjawa@gmail.com>
2023-04-29 22:09:52 +0700

    quotes responsive

--- src/components/CardQuote.jsx
+++ src/components/CardQuote.jsx
@@ -3,18 +3,18 @@ import "../styles/CardQuote.css";
 
 const CardQuote = ({ quotes }) => {
 	return (
-		<div>
+		<div className="flex">
 			{/* card quote */}
-			<div className="con-card w-[368px] h-[397px] relative flex justify-center">
-				<div className="rounded-full w-[140px] h-[140px] overflow-hidden absolute z-20 flex justfy-center flex-wrap">
+			<div className="con-card-quote w-[368px] h-auto max-h-[391px] relative flex justify-center">
+				<div className="card-qoute-img rounded-full w-[140px] h-[140px] overflow-hidden absolute z-20 flex justfy-center	">
 					<img className="object-cover" src={quotes.img} alt={quotes.name} />
 				</div>
-				<div className="card-quotes bg-white w-[368px] h-[368px]  rounded-[15px]  self-end flex flex-col justify-center items-center">
-					<div className="absolute bottom-[20px] flex flex-col gap-[19px]">
-						<p className="w-[328px] h-[147px] italic leading-[130%] text-center text-[16px] flexjustify-center items-center text-[#626262] font-['poppins']">
+				<div className="card-quote bg-white w-full h-full min-w[300px] min-h[368px] max-w-[368px] max-h-[368px]  rounded-[15px]  self-end flex flex-col justify-center items-center relative">
+					<div className="quote w-auto h-auto absolute bottom-[20px] flex flex-col gap-[19px] p-3">
+						<p className="h-auto w-full italic leading-[130%] text-center text-[16px] flex justify-center items-center text-[#626262] font-['poppins']">
 							"{quotes.quote}"
 						</p>
-						<div className="flex justify-center items-center gap-[8px] flex-col w-[328px] h-[42]px">
+						<div className="flex justify-center items-center gap-[8px] flex-col w-full h-[42]px">
 							<h3 className="font-bold tracking-[0.07em] font-['poppins']">
 								{quotes.name}
 							</h3>
--- src/components/Home/Quotes.jsx
+++ src/components/Home/Quotes.jsx
@@ -10,9 +10,9 @@ const Quotes = () => {
 				<h1 className=" font-bold text-[24px] tracking-[0.07em] text-[#212125]">
 					APA KATA CUSTOMER KITA
 				</h1>
-				<div className="hide-scroll pl[98px] h-[450px] max-w-[1152px] w-[100%] flex relative overflow-x-scroll gap-[24px] min-w-300">
-					<span className="gradien-slide w-[98px] h-[397px] flex z-30 absolute left-0 "></span>
-					<span className="gradien-slide w-[98px] h-[397px] flex z-30 absolute -right-1 rotate-180 "></span>
+				<div className="hide-scroll h-[450px] max-w-[1152px] w-[100%] flex relative overflow-x-scroll gap-[24px] min-w-300">
+					<span className="gradien-slide w-[98px] h-full flex z-30 absolute left-0 "></span>
+					<span className="gradien-slide w-[98px] h-full flex z-30 absolute -right-1 rotate-180 "></span>
 					<div className="hide-scroll pl-[98px] pr-[98px] h-[450px]max-w-[1152px] w-[100%] flex relative overflow-x-scroll gap-[24px] min-w-300">
 						{quotes.map((quotes) => (
 							<CardQuote key={quotes.no} quotes={quotes} />
