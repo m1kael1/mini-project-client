328c194fc7ff30bc38058d5f1e624c52f8ab5e76
m1kael1 <mikaelagungdjawa@gmail.com>
2023-05-31 20:36:38 +0700

    repair

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -7,7 +7,7 @@ const CardProduct = ({ item }) => {
 		<div>
 			{/* card-product */}
 			<div className="card-product w-[270px] p-4  bg-white rounded-[10px] shadow-[0px_0px_15px_rgba(0,0,0,0.25)] flex-col items-center flex">
-				<Link to={`/product/${item.id}`} className="w-full">
+				<Link to={`/product/${item.id}`} className="w-full hover:text-black">
 					<div className="img-product-container max-w-[222px] w-full h-[157px] rounded-[10px] mb-[16px] mt-[21px] mb-[16px] flex justfy-center item center flex-wrap ">
 						<img
 							className="object-cover w-full h-full"
@@ -35,9 +35,9 @@ const CardProduct = ({ item }) => {
 					<span className="max-w-[73px] w-full h-[23px] p-2 bg-[#239D60] rounded-[10px] flex justify-center items-center font-['Poppins'] text-[12px] capitalize">
 						{item.categories2}
 					</span>
-					<a href="/" className="min-w-[32px] max-w-[32px] w-full h-[32px]">
+					<Link to="" className="min-w-[32px] max-w-[32px] w-full h-[32px]">
 						<img src={whatsapplogo} alt="Whatsapp" />
-					</a>
+					</Link>
 				</div>
 			</div>
 			{/* end-card */}
