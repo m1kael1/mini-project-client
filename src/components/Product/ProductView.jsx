30f8bae034b0361b4cef480f5c2fba8964621560
m1kael1 <mikaelagungdjawa@gmail.com>
2023-07-25 20:01:12 +0700

    modified

--- src/components/Product/ProductView.jsx
+++ src/components/Product/ProductView.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
-import SearchLayout from "../../layouts/SearchLayout";
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import whatsapplogo from "../../assets/whatsapp-logo.svg";
 
-const ProductView = ({ item }) => {
-	const params = useParams();
-	const testcall = item[params.id - 1];
-
-	// untuk list desc
-	const list = testcall.desc;
+const ProductView = ({ products }) => {
+	const productName = useParams().name;
+	const product = products.find((product) => product.name === productName);
+	const listDesc = product.desc;
 
 	return (
 		<>
@@ -19,42 +16,42 @@ const ProductView = ({ item }) => {
 						<div className="max-w-[562px] w-full h-[515px] border-solid border-[1px] rounded-[15px] border-#9A9A9A flex justify-center overflow-hidden">
 							<img
 								className="object-contain "
-								src={testcall.img}
-								alt={testcall.name}
+								src={product.img}
+								alt={product.name}
 							/>
 						</div>
 					</div>
 					<div className="flex flex-col h-auto gap-12">
 						<h1 className="uppercase text-[34px] font-['poppins'] tracking-[0.07em] ">
-							{testcall.name}
+							{product.name}
 						</h1>
 						<div className="flex flex-col gap-[15px]">
 							<span className="list-none font-['poppins'] text-[34px] font-bold text-[#239D60] tracking-[0.07em]">
-								RP. {testcall.price}
+								RP. {product.price}
 							</span>
 							<li className="list-none font-['poppins'] text-[20px] tracking-[0.07em]">
-								Stock: {testcall.stok}
+								Stock: {product.stok}
 							</li>
 							<li className="list-none font-['poppins'] text-[20px] tracking-[0.07em]">
 								Brand:{" "}
 								<span className="capitalize font-['poppins'] text-[20px] tracking-[0.07em]">
-									{testcall.brand}
+									{product.brand}
 								</span>
 							</li>
 							<li className="list-none font-['poppins'] text-[20px] tracking-[0.07em]">
 								Kategori:{" "}
 								<span className="capitalize font-['poppins'] tracking-[0.07em] ">
-									{testcall.categories2}
+									{product.categories2}
 								</span>
 							</li>
 						</div>
 						<div className="w-[368px] h-[42px] flex justify-between">
-							<a
-								href="/"
+							<Link
+								to="https://wa.me/+6282145764595"
 								className="w-[172px] h-[42px] flex items-center border-[2px] border-solid gap-[27px] border-[#239D60] rounded-[10px] justify-center font-['poppins'] tracking-[0.07em] hover:bg-[#239D60] hover:text-white transition duration-150 ease-in-out"
 							>
-								CHAT VIA <img src={whatsapplogo} alt="" />
-							</a>
+								CHAT VIA <img src={whatsapplogo} alt="Chat on WhatsApp" />
+							</Link>
 							<Link
 								to="/contact"
 								className="w-[172px] h-[42px] flex items-center bg-[#1170FF] text-white rounded-[10px] justify-center tracking-[0.07em] hover:bg-[#5799FD] transition duration-150 ease-in-out"
@@ -72,7 +69,7 @@ const ProductView = ({ item }) => {
 							DESKRIPSI PRODUK
 						</h1>
 						<ul className="w-full h-[412px] p-0 ">
-							{list.map((list, index) => (
+							{listDesc.map((list, index) => (
 								<li key={index} className="list-none font-['poppins']">
 									{list}
 								</li>
